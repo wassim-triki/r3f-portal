bfd328e76d3f026f1b3309811c53f784668e684a
Wassim Triki <wsm@gmail.com>
2023-09-12 16:30:50 +0100

    add route wild card

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/item/:id" element={<App />} />
         <Route path="/" element={<App />} />
+        <Route path="/*" element={<Navigate to={"/"} />} />
       </Routes>
     </Router>
   </React.StrictMode>
